1b995c6ef2a09a2570286dc1b6b885a5573567ff
Dan Nilsson <dan.nilsson@king.com?>
2014-08-15 10:24:34 +0200

    - Added datatool main

---
+++ datatool/src/datatool_main.cpp
@@ -0,0 +1,13 @@
+#include <putki/builder/build.h>
+#include <putki/builder/builder.h>
+#include <putki/liveupdate/liveupdate.h>
+#include <putki/runtime.h>
+#include <stdint.h>
+#include <iostream>
+
+int run_putki_datatool(int argc, char **argv)
+{
+	std::cout << "Putki data tool." << std::endl;
+	
+	return 0;
+}
