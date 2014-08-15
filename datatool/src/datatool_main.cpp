9ba274e7e298deb5b5b551c1c6f64356fb3e4195
Dan Nilsson <dan.nilsson@king.com?>
2014-08-15 11:00:29 +0200

    - Data tool extras

--- datatool/src/datatool_main.cpp
+++ datatool/src/datatool_main.cpp
@@ -1,24 +1,51 @@
 #include <putki/builder/build.h>
 #include <putki/builder/builder.h>
 #include <putki/builder/typereg.h>
+#include <putki/builder/write.h>
+#include <putki/builder/db.h>
 #include <putki/liveupdate/liveupdate.h>
 #include <putki/runtime.h>
 #include <stdint.h>
 #include <iostream>
 
-int run_putki_datatool(int argc, char **argv)
+void make_data(const char *name)
 {
-	std::cout << "Putki data tool!" << std::endl;
-	
-	for (unsigned int i=0;true;i++) {
-		putki::type_handler_i *type = putki::typereg_get_handler_by_index(i);
-		if (!type) {
-			std::cout << "Got a total of " << i << " types." << std::endl;
-			break;
-		}
+	putki::type_handler_i *type = putki::typereg_get_handler(name);
+	if (!type) {
+		std::cerr << "No type with name '" << name << "'" << std::endl;
+		return;
 	}
 
-	
+	putki::instance_t obj = type->alloc();
 
+	const char *path = "tmp_path";
+	putki::db::data *db = putki::db::create();
+	putki::db::insert(db, path, type, obj);
+	putki::write::write_object_into_stream(std::cout, db, type, obj);
+
+	putki::db::free_and_destroy_objs(db);
+}
+
+int run_putki_datatool(int argc, char **argv)
+{
+	for (int i=1;i<argc;i++) {
+		if (!strcmp(argv[i], "make")) {
+			if (i+1 < argc) {
+				make_data(argv[i+1]);
+			} else {
+				std::cerr << "Syntax: make <type>" << std::endl;
+			}
+		} else if (!strcmp(argv[i], "list-types")) {
+			for (unsigned int i=0;true;i++) {
+				putki::type_handler_i *type = putki::typereg_get_handler_by_index(i);
+				if (!type) {
+					std::cout << "Total: " << i << " types." << std::endl;
+					break;
+				} else {
+					std::cout << type->name() << std::endl;
+				}
+			}
+		}
+	}
 	return 0;
 }
