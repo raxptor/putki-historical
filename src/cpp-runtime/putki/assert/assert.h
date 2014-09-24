43594bba2033febe85473795e4e127136fc44202
Dan Nilsson <raxptor@gmail.com>
2014-09-24 16:36:40 +0200

    - Added assert

---
+++ src/cpp-runtime/putki/assert/assert.h
@@ -0,0 +1,28 @@
+#ifndef __PUTKI_RT_ASSERT_H__
+#define __PUTKI_RT_ASSERT_H__
+
+#include <putki/log/log.h>
+
+#if true || defined(PUTKI_ENABLE_ASSERT)
+
+	#define PTK_LOG(type, stmt) { \
+		std::stringstream __DPRINT_LINE; \
+		__DPRINT_LINE << __FILE__ << " (" << __LINE__ << "): " << stmt; \
+		putki::log(type, __DPRINT_LINE.str().c_str()); \
+	} \
+
+	#define PTK_ASSERT(x) if (!(x)) PTK_WARNING("Assertion failed!")
+	#define PTK_ASSERT_DESC(x, y) if (!(x)) PTK_WARNING("Assertion failed: " << y)
+	#define PTK_FATAL_ASSERT(x) if (!(x)) PTK_ERROR("Fatal assert!")
+	#define PTK_FATAL_ASSERT_DESC(x, y) if (!(x)) PTK_ERROR("Fatal assert: " << y)
+
+#else
+
+	#define PTK_ASSERT(x) {}
+	#define PTK_ASSERT_DESC(x, y) {}
+	#define PTK_FATAL_ASSERT(x) {}
+	#define PTK_FATAL_ASSERT_DESC(x, y) {}
+
+#endif
+
+#endif
