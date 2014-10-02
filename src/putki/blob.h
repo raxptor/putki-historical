d0c3b5b403b14947a17a244e9d4d1b55e87d7cbb
Dan Nilsson <dan@datacraft.se>
2014-10-02 21:52:46 +0200

    - fixed 64 bit

--- src/putki/blob.h
+++ src/putki/blob.h
@@ -24,7 +24,11 @@ namespace putki
 		where[1] = (val >> 8) & 0xff;
 		where[2] = (val >> 16) & 0xff;
 		where[3] = (val >> 24) & 0xff;
-		return where + 4;
+		where[4] = (val >> 32) & 0xff;
+		where[5] = (val >> 40) & 0xff;
+		where[6] = (val >> 48) & 0xff;
+		where[7] = (val >> 56) & 0xff;
+		return where + 8;
 	}
 
 	char *pack_string_field(int size_size, char *where, const char *src, char *aux_beg, char *aux_end);
