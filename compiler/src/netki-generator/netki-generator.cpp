f383c1c3e48ad1509beecac9cf43d4f653d7fcdf
Dan Nilsson <raxptor@gmail.com>
2015-02-05 13:15:22 +0100

    - Netki bit writing and reading, buffer flipping
    TODO: Add range checking

--- compiler/src/netki-generator/netki-generator.cpp
+++ compiler/src/netki-generator/netki-generator.cpp
@@ -10,10 +10,41 @@ namespace putki
 {
 	bool is_netki_struct(parsed_struct *s)
 	{
-		for (int i=0;i!=s->targets.size();i++)
+		for (int i=0; i!=s->targets.size(); i++)
 			if (!strcmp(s->targets[i].c_str(), "netki"))
 				return true;
 		return false;
+	}
+
+	std::string field_type(parsed_field *field)
+	{
+		std::string type = "err";
+		switch (field->type)
+		{
+			case FIELDTYPE_BOOL:
+				type = "bool";
+				break;
+			case FIELDTYPE_INT32:
+				type = "uint32_t";
+				break;
+			case FIELDTYPE_BYTE:
+				type = "uint8_t";
+				break;
+			case FIELDTYPE_STRING:
+				type = "const char*";
+				break;
+			case FIELDTYPE_POINTER:
+				type = field->ref_type + "*";
+				break;
+			case FIELDTYPE_ENUM:
+			case FIELDTYPE_STRUCT_INSTANCE:
+				type = field->ref_type;
+				break;
+			default:
+				type = "unknown!";
+				break;
+		}
+		return type;
 	}
 
 	void write_struct_header(parsed_struct *s, indentedwriter &wr)
@@ -30,37 +61,11 @@ namespace putki
 		indentedwriter cw(constructor);
 		cw.indent(3);
 
-		for (int i=0;i!=s->fields.size();i++)
+		for (int i=0; i!=s->fields.size(); i++)
 		{
 			parsed_field *field = &s->fields[i];
 
-			std::string type = "err";
-			switch (field->type)
-			{
-				case FIELDTYPE_BOOL:
-					type = "bool";
-					break;
-				case FIELDTYPE_INT32:
-					type = "uint32_t";
-					break;
-				case FIELDTYPE_BYTE:
-					type = "uint8_t";
-					break;
-				case FIELDTYPE_STRING:
-					type = "const char*";
-					break;
-				case FIELDTYPE_POINTER:
-					type = field->ref_type + "*";
-					break;
-				case FIELDTYPE_ENUM:
-				case FIELDTYPE_STRUCT_INSTANCE:
-					type = field->ref_type;
-					break;
-				default:
-					type = "unknown!";
-					break;
-			}
-
+			std::string type = field_type(field);
 			std::string defval = field->def_value;
 			if (defval.empty())
 				defval = "0";
@@ -82,7 +87,7 @@ namespace putki
 		wr.line() << "// Automatically generated fields";
 		wr.line() << "static inline int type_id() { return " << s->unique_id << "; }";
 		wr.line() << "static bool write_into_bitstream(" << s->name << " *source, netki::bitstream::buffer *dest);";
-		wr.line() << "static bool parse_from_bitstream(" << s->name << " *target, netki::bitstream::buffer *dest);";
+		wr.line() << "static bool parse_from_bitstream(" << s->name << " *target, netki::bitstream::buffer *source, netki::bitstream::buffer *allocbuf);";
 		wr.line();
 		wr.line() << s->name << "()";
 		wr.line() << "{";
@@ -101,7 +106,7 @@ namespace putki
 		wr.line() << "{";
 		wr.indent(1);
 
-		for (int i=0;i!=s->fields.size();i++)
+		for (int i=0; i!=s->fields.size(); i++)
 		{
 			parsed_field *field = &s->fields[i];
 
@@ -129,8 +134,19 @@ namespace putki
 					wr.line() << "bitstream::insert_bits<8>(dest, " << field_ref << ");";
 					break;
 				case FIELDTYPE_STRING:
+					wr.line() << "if (" << field_ref << ")";
+					wr.line() << "{";
+					wr.indent(1);
 					wr.line() << "bitstream::insert_bits<16>(dest, strlen(" << field_ref << "));";
 					wr.line() << "bitstream::insert_bytes(dest, (uint8_t*) " << field_ref << ", strlen(" << field_ref << "));";
+					wr.indent(-1);
+					wr.line() << "}";
+					wr.line() << "else";
+					wr.line() << "{";
+					wr.indent(1);
+					wr.line() << "bitstream::insert_bits<16>(dest, 0xffff);";
+					wr.line() << "}";
+					wr.indent(1);
 					break;
 				case FIELDTYPE_ENUM:
 					wr.line() << "bitstream::insert_bits<32>(dest, (int)" << field_ref << ");";
@@ -157,9 +173,86 @@ namespace putki
 		wr.indent(-1);
 		wr.line() << "}";
 		wr.line();
-		wr.line() << "bool " << s->name << "::parse_from_bitstream(" << s->name << " *source, netki::bitstream::buffer *dest)";
+		wr.line() << "bool " << s->name << "::parse_from_bitstream(" << s->name << " *dest, netki::bitstream::buffer *source, netki::bitstream::buffer *unpackbuf)";
 		wr.line() << "{";
 		wr.indent(1);
+
+		// parsers
+		for (int i=0; i!=s->fields.size(); i++)
+		{
+			parsed_field *field = &s->fields[i];
+
+			std::string field_ref = std::string("dest->") + field->name;
+			std::string type = field_type(field);
+
+			if (field->is_array)
+			{
+				wr.line() << "{";
+				wr.indent(1);
+				wr.line() << field_ref << "_size = bitstream::read_bits<32>(source);";
+				wr.line() << field_ref << " = (" << type << " *)bitstream::alloc(unpackbuf, sizeof(" << type << ") * " << field_ref << "_size);";
+				wr.line() << "if (!" << field_ref << ")";
+				wr.line(1) << "return false;";
+				wr.line() << "for (int i=0;i!=" << field_ref << "_size;i++)";
+				wr.line() << "{";
+				wr.indent(1);
+				field_ref.append("[i]");
+			}
+
+			switch (field->type)
+			{
+				case FIELDTYPE_BOOL:
+					wr.line() << field_ref << " = bitstream::read_bits<1>(source);";
+					break;
+				case FIELDTYPE_INT32:
+					wr.line() << field_ref << " = (uint32_t) bitstream::read_bits<32>(source);";
+					break;
+				case FIELDTYPE_BYTE:
+					wr.line() << field_ref << " = bitstream::read_bits<8>(source);";
+					break;
+				case FIELDTYPE_STRING:
+					wr.line() << "{";
+					wr.indent(1);
+					wr.line() << "uint32_t len = (uint32_t) bitstream::read_bits<16>(source);";
+					wr.line() << "if (len == 0xffff)";
+					wr.line() << "{";
+					wr.line(1) << field_ref << " = 0;";
+					wr.line() << "}";
+					wr.line() << "else";
+					wr.line() << "{";
+					wr.indent(1);
+					wr.line() << "uint8_t *buf = (uint8_t*) bitstream::alloc(unpackbuf, len + 1);";
+					wr.line() << field_ref << " = (const char*) buf;";
+					wr.line() << "if (!" << field_ref << ")";
+					wr.line(1) << "return false;";
+					wr.line() << "bitstream::read_bytes(source, buf, len);";
+					wr.line() << "buf[len] = 0;";
+					wr.indent(-1);
+					wr.line() << "}";
+					wr.indent(-1);
+					wr.line() << "}";
+					break;
+				case FIELDTYPE_ENUM:
+					wr.line() << field_ref << " = (" << type << ") = bitstream::read_bits<32>(source);";
+					break;
+				case FIELDTYPE_STRUCT_INSTANCE:
+					wr.line() << "if (!" << field->ref_type << "::parse_from_bitstream(&" << field_ref << ", source, unpackbuf))";
+					wr.line(1) << "return false;";
+					break;
+				default:
+					wr.line() << "<compile error trying to write field " << s->name << ">";
+					break;
+			}
+
+			if (field->is_array)
+			{
+				wr.indent(-1);
+				wr.line() << "}";
+				wr.indent(-1);
+				wr.line() << "}";
+			}
+		}
+
 		wr.line() << "return false;";
 		wr.indent(-1);
 		wr.line() << "}";
@@ -176,7 +269,7 @@ namespace putki
 
 		bool write_master = false;
 
-		for (int i=0;i!=proj->files.size();i++)
+		for (int i=0; i!=proj->files.size(); i++)
 		{
 			parsed_file *pf = &proj->files[i];
 			std::string subpath = pf->sourcepath.substr(proj->base_path.size()+1);
@@ -194,7 +287,7 @@ namespace putki
 				hw.indent(1);
 				hw.line() << "namespace bitstream { struct buffer; }";
 
-				for (int j=0;j!=pf->structs.size();j++)
+				for (int j=0; j!=pf->structs.size(); j++)
 				{
 					parsed_struct *s = &pf->structs[j];
 					if (is_netki_struct(s))
@@ -223,13 +316,11 @@ namespace putki
 				iw.line() << "{";
 				iw.indent(1);
 
-				for (int j=0;j!=pf->structs.size();j++)
+				for (int j=0; j!=pf->structs.size(); j++)
 				{
 					parsed_struct *s = &pf->structs[j];
 					if (is_netki_struct(s))
-					{
 						write_struct_impl(s, iw);
-					}
 				}
 				iw.indent(-1);
 				iw.line() << "}";
@@ -240,9 +331,7 @@ namespace putki
 		}
 
 		if (write_master)
-		{
 			putki::save_stream(out_base + "/" + proj->module_name + "-netki-runtime-master.cpp", netki_master);
-		}
 	}
 
 }
--- src/cpp-runtime/netki/bitstream.h
+++ src/cpp-runtime/netki/bitstream.h
@@ -8,34 +8,133 @@ namespace netki
 	namespace bitstream
 	{
 		typedef int bitofs_t;
-		
+
 		struct buffer
 		{
-			uint8_t buf[512];
+			uint8_t *buf;
+			uint32_t bufsize;
 			bitofs_t bitpos;
 			int bytepos;
 		};
-		
+
+		inline void flip_buffer(buffer *buf)
+		{
+			buf->bufsize = buf->bytepos + 1;
+			buf->bitpos = 0;
+			buf->bytepos = 0;
+		}
+
+		template<int size>
+		inline void init_buffer(buffer *buf, char(&data)[size])
+		{
+			buf->bufsize = size;
+			buf->buf = (uint8_t*)data;
+			buf->bitpos = 0;
+			buf->bytepos = 0;
+		}
+
+		static const uint8_t bitmask[9] = {0x0, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, 0x7f, 0xff};
+
+		inline long bits_left(buffer *buf)
+		{
+			return (buf->bufsize - buf->bytepos) * 8 - buf->bitpos;
+		}
+
+		inline long bytes_left(buffer *buf)
+		{
+			return buf->bufsize - buf->bytepos - 1;
+		}
+
 		inline void set_bits(uint8_t *in, bitofs_t ofs, uint8_t value)
 		{
 			(*in) = (*in) | (value << ofs);
 		}
-		
+
 		template<int bits>
-		inline void insert_bits(buffer *target, uint8_t value)
+		inline uint8_t get_bits(uint8_t *in, bitofs_t ofs)
 		{
+			return ((*in) >> ofs) & bitmask[bits];
+		}
+
+		template<int bits>
+		inline void insert_bits(buffer *target, uint32_t value)
+		{
+			if (bits > 8)
+			{
+				insert_bits<8>(target, value & 0xff);
+				insert_bits<bits - 8>(target, value >> 8);
+				return;
+			}
+
 			const bitofs_t left = (8 - target->bitpos);
-			set_bits(&target->buf[target->bytepos], value, target->bitpos);
+			set_bits(&target->buf[target->bytepos], target->bitpos, value);
 
 			bitofs_t np = target->bitpos + bits;
-			
+
 			if (bits > left)
-				set_bits(&target->buf[target->bytepos+1], 0, bits - left);
-			
+				set_bits(&target->buf[target->bytepos+1], 0, value >> left);
+
 			target->bytepos = target->bytepos + (np >> 3);
-			target->bitpos = np & 7;		
+			target->bitpos = np & 7;
 		}
-		
+
+		template<> inline void insert_bits<0>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-1>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-2>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-3>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-4>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-5>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-6>(buffer *target, uint32_t value) { }
+		template<> inline void insert_bits<-7>(buffer *target, uint32_t value) { }
+
+		inline uint32_t read_bits(buffer *source, unsigned int count);
+
+		// MAX bits is 8
+		template<int bits>
+		inline uint32_t read_bits(buffer *source)
+		{
+			if (bits > 8)
+				// this comes back with smaller pieces.
+				return read_bits(source, bits);
+
+			const bitofs_t left = (8 - source->bitpos);
+
+			uint32_t fetch_first = left < bits ? left : bits;
+
+			uint32_t value = (source->buf[source->bytepos] >> source->bitpos) & bitmask[fetch_first];
+
+			bitofs_t np = source->bitpos + bits;
+			source->bytepos = source->bytepos + (np >> 3);
+			source->bitpos = np & 7;
+
+			if (fetch_first != bits)
+			{
+				uint32_t remaining = bits - fetch_first;
+				value = value | ((source->buf[source->bytepos] & bitmask[remaining]) << fetch_first);
+				source->bitpos = remaining;
+			}
+
+			return value;
+		}
+
+		inline uint32_t read_bits(buffer *source, unsigned int count)
+		{
+			switch (count)
+			{
+				case 0: return 0;
+				case 1: return read_bits<1>(source);
+				case 2: return read_bits<2>(source);
+				case 3: return read_bits<3>(source);
+				case 4: return read_bits<4>(source);
+				case 5: return read_bits<5>(source);
+				case 6: return read_bits<6>(source);
+				case 7: return read_bits<7>(source);
+				case 8: return read_bits<8>(source);
+				default:
+					return read_bits<8>(source) | (read_bits(source, count - 8) << 8);
+			}
+		}
+
 		inline void sync_byte(buffer *target)
 		{
 			if (target->bitpos)
@@ -44,13 +143,37 @@ namespace netki
 				target->bitpos = 0;
 			}
 		}
-		
+
 		inline void insert_bytes(buffer *target, uint8_t *buf, int size)
 		{
-			sync_byte(target);
-			for (int i=0;i!=size;i++)
-				target->buf[target->bytepos + i] = buf[i];
-			target->bytepos += size; 
+			if (bytes_left(target) >= size)
+			{
+				sync_byte(target);
+				for (int i=0; i!=size; i++)
+					target->buf[target->bytepos + i] = buf[i];
+				target->bytepos += size;
+			}
+		}
+
+		inline void read_bytes(buffer *source, uint8_t *buf, int size)
+		{
+			if (bytes_left(source) >= size)
+			{
+				sync_byte(source);
+				for (int i=0; i!=size; i++)
+					buf[i] = source->buf[source->bytepos + i];
+				source->bytepos += size;
+			}
+		}
+
+		inline void *alloc(buffer *target, uint32_t size)
+		{
+			if (bytes_left(target) < size)
+				return 0;
+
+			void *ptr = &target->buf[target->bytepos];
+			target->bytepos += size;
+			return ptr;
 		}
 	}
 }
