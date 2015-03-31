51593af925b3f5c374b905cec4319bc8ab81f695
Dan Nilsson <raxptor@gmail.com>
2015-03-31 10:29:02 +0200

    - java interop

--- putked/interopdll/premake4.lua
+++ putked/interopdll/premake4.lua
@@ -1,16 +1,27 @@
-solution "MonoEdInterop"
-
+solution "PutkEdInteropMono"
 	platforms { "x32" }
 	flags {"StaticRuntime"}
-
 	configurations "Release"
-
 	project "monoedinterop"
 
 		kind "SharedLib"	
 		language "C++"
 		targetname "monoed-interop"
 
-		files { "interoplib.cpp" }
+		files { "interoplib-mono.cpp" }
+		files { "../../src/data-dll/**.h" }
+		includedirs { "../../src/data-dll/putki" }
+
+solution "PutkEdInteropJava"
+	platforms { "x64" }
+	flags {"StaticRuntime"}
+	configurations "Release"
+	project "interopjava"
+
+		kind "SharedLib"	
+		language "C++"
+		targetname "putked-java-interop"
+
+		files { "interoplib-java.cpp" }
 		files { "../../src/data-dll/**.h" }
 		includedirs { "../../src/data-dll/putki" }
