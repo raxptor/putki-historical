8fa69a840ab50fbbec9ee50c8f202f8c85099842
Dan Nilsson <dan.nilsson@king.com>
2015-05-21 16:16:14 +0200

    - fix builder

--- putked/editor/src/putked/DefaultBuildLoader.java
+++ putked/editor/src/putked/DefaultBuildLoader.java
@@ -14,6 +14,7 @@ public class DefaultBuildLoader implements EditorPluginDescription
 		m_interop = interop;
 		m_user = user;
 		m_extras = extras;
+		m_builder = builder;
 	}
 	
 	@Override
@@ -46,18 +47,23 @@ public class DefaultBuildLoader implements EditorPluginDescription
 			File whereTo = Files.createTempDirectory("loader").toFile();
 			File s1 = new File(whereTo, m_interop);
 			File s2 = new File(whereTo, m_user);
-			File s3 = new File(whereTo, m_builder);
+    		File s3 = new File(whereTo, m_builder);
+    		    System.out.println("Copying interop...");
 		    InputStream link1 = open("/putked/native/" + m_interop);		    
 		    Files.copy(link1, s1.getAbsoluteFile().toPath());
+    		    System.out.println("Copying data dll...");
 		    InputStream link2 = open("/native/" + m_user);
 		    Files.copy(link2, s2.getAbsoluteFile().toPath());		    
-		    InputStream link3 = open("/native/" + m_user);
+    		    System.out.println("Copying builder exe...");
+		    InputStream link3 = open("/native/" + m_builder);
 		    Files.copy(link3, s3.getAbsoluteFile().toPath());		    
+    		    System.out.println("Copying extras...");
 		    for (String extra : m_extras) {
-				File sE = new File(whereTo, m_user);
+			File sE = new File(whereTo, m_user);
 		    	InputStream linkE = open("/native/" + extra);
 		    	Files.copy(linkE, sE.getAbsoluteFile().toPath());	
 		    }
+    		    System.out.println("Doing interop init");
 		    Main.interopInit(s1.getAbsolutePath(),  s2.getAbsolutePath(), s3.getAbsolutePath());
 		}
 		catch (Exception e) {
